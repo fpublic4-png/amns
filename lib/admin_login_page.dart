a74ed3cc16858ae4bb39ac83245b31adca65c766
fpublic4 <fpublic4@gmail.com>
2025-12-18 09:22:50 +0000

    yes

---
+++ lib/admin_login_page.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class AdminLoginPage extends StatelessWidget {
+  const AdminLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Login'),
+      ),
+      body: const Center(
+        child: Text('Admin Login Page'),
+      ),
+    );
+  }
+}
---
+++ lib/teacher_login_page.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class TeacherLoginPage extends StatelessWidget {
+  const TeacherLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Teacher Login'),
+      ),
+      body: const Center(
+        child: Text('Teacher Login Page'),
+      ),
+    );
+  }
+}
